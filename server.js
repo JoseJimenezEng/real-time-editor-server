efab0bad6f0f84d1fb33bb6a2423b4ac129eca50
JoseJimenezEng <jjmarts@gmail.com>
2024-08-16 23:25:39 -0500

    added env vars, users and rooms

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const http = require('http');
 const socketIo = require('socket.io');
 const cors = require('cors');
+require('dotenv').config();
 
 const app = express();
 
@@ -18,19 +19,34 @@ const io = socketIo(server, {
   }
 });
 
+let rooms = {};
 
 io.on('connection', (socket) => {
   console.log('New client connected');
+  socket.on('join room', ({ username, room }) => {
+    socket.join(room);
+    if (!rooms[room]) {
+      rooms[room] = [];
+    }
+    rooms[room].push({ id: socket.id, username });
 
-  socket.on('text update', (text) => {
-    socket.broadcast.emit('text update', text);
+    io.to(room).emit('room data', rooms[room]);
+    console.log(`${username} joined room: ${room}`);
+  });
+
+  socket.on('text update', ({ text, room }) => {
+    socket.to(room).emit('text update', text);
   });
 
   socket.on('disconnect', () => {
+    for (let room in rooms) {
+      rooms[room] = rooms[room].filter(user => user.id !== socket.id);
+      io.to(room).emit('room data', rooms[room]);
+    }
     console.log('Client disconnected');
   });
 });
 
-server.listen(4000, () => {
-  console.log('Server is running on port 4000');
+server.listen(process.env.PORT, () => {
+  console.log('Server is running');
 });
